16d496d99501962aed189024d88327906237fdec
yshan2 <280028879@qq.com>
2017-05-30 02:38:21 +0800

    5。29 from chuanzhi python6th's learning
    Signed-off-by: yshan2 <280028879@qq.com>

--- search.py
+++ search.py
@@ -1,39 +1,39 @@
-def src(l,n,s=0):
-	e = len(l)-1
+def src(l, n, s=0):
+	e = len(l) - 1
 	if l[e] == n:
 		return e
 	elif l[0] == n:
 		return 0
-	return srch(l,s+1,e-1,n)
+	return srch(l, s + 1, e - 1, n)
 
-def srch(l,s,e,n):
+
+def srch(l, s, e, n):
 	# if len(l)<1:
 	# 	print('not found. ')
 	# 	return -1
-	m = (e-s)//2 + s
+	m = (e - s) // 2 + s
 	a = l[m]
 	if n == l[e]:
 		return e
 	elif n == l[s]:
 		return s
-	elif n>a:
-		if n<l[e]:
-			s2 = m  + 1
-			return srch(l,s2,e,n)
+	elif n > a:
+		if n < l[e]:
+			s2 = m + 1
+			return srch(l, s2, e, n)
 		else:
-			e2 = m  - 1
-			return srch(l,s,e2,n)
+			e2 = m - 1
+			return srch(l, s, e2, n)
 	elif n == a:
 		return m
-	elif n<a:
+	elif n < a:
 		e2 = m - 1
-		return srch(l,s,e2,n)
+		return srch(l, s, e2, n)
 	# elif len(l)<3:
 	# 	print('not found. ')
 	# 	return -1
 
-l = [17,18,19,0,1,2,6]
+
+l = [17, 18, 19, 0, 1, 2, 6]
 print(l)
-print(src(l,11))
-
-
+print(src(l, 11))
